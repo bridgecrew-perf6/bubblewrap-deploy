294bb330607ef5cbcc610d8d1853ca771a8209b6
umairx97 <umairahmedbajwa97@gmail.com>
2022-06-24 00:31:45 +0500

    feat: generate barebones bubblewrap outlier app

---
+++ app/src/main/java/org/outlier/collegesuccess_pwa/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package org.outlier.collegesuccess_pwa.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
